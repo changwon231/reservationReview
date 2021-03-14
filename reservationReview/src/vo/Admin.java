3e81edd0c7aaf7a08db54e7268f5c04bd2d3fdb9
changwon231 <tlsckdnjs@naver.com>
2021-03-14 14:21:04 +0900

    두번째 커밋

--- reservationReview/src/vo/Admin.java
+++ reservationReview/src/vo/Admin.java
@@ -2,4 +2,141 @@ package vo;
 
 public class Admin {
 
+	// 변수 선언
+	private int adminSeq;
+	private String adminId;
+	private String name;
+	private String password;
+	private int adminType;
+	private String email;
+	private String phone;
+	private int regId;
+	private String regDate;
+	private String modId;
+	private String modDate;
+	
+	// 기본 생성자
+	public Admin() {
+		super();
+	}
+
+	// getter, setter
+	public int getAdminSeq() {
+		return adminSeq;
+	}
+
+	public void setAdminSeq(int adminSeq) {
+		this.adminSeq = adminSeq;
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAdminType() {
+		return adminType;
+	}
+
+	public void setAdminType(int adminType) {
+		this.adminType = adminType;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getRegId() {
+		return regId;
+	}
+
+	public void setRegId(int regId) {
+		this.regId = regId;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getModId() {
+		return modId;
+	}
+
+	public void setModId(String modId) {
+		this.modId = modId;
+	}
+
+	public String getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(String modDate) {
+		this.modDate = modDate;
+	}
+	
+	// hash, equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + adminSeq;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin other = (Admin) obj;
+		if (adminSeq != other.adminSeq)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "관리자 [관리자 일련번호=" + adminSeq + ", 관리자 아이디=" + adminId + ", 이름=" + name + ", 비밀번호=" + password
+				+ ", 관리자 타입=" + adminType + ", 이메일=" + email + ", 전화번호=" + phone + ", 등록자 아이디=" + regId
+				+ ", 등록일=" + regDate + ", 수정 아이디=" + modId + ", 수정일=" + modDate + "]";
+	}	
+	
 }
--- reservationReview/src/vo/Customer.java
+++ reservationReview/src/vo/Customer.java
@@ -2,4 +2,151 @@ package vo;
 
 public class Customer {
 
+	//변수 선언
+	private int custSeq;
+	private String custId;
+	private String name;
+	private String password;
+	private int custType;
+	private int custNb;
+	private String email;
+	private String phone;
+	private String regId;
+	private String regDate;
+	private String modId;
+	private String modDate;
+	
+	// 기본 생성자
+	public Customer() {
+		super();
+	}
+
+	// getter, setter
+	public int getCustSeq() {
+		return custSeq;
+	}
+
+	public void setCustSeq(int custSeq) {
+		this.custSeq = custSeq;
+	}
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getCustType() {
+		return custType;
+	}
+
+	public void setCustType(int custType) {
+		this.custType = custType;
+	}
+
+	public int getCustNb() {
+		return custNb;
+	}
+
+	public void setCustNb(int custNb) {
+		this.custNb = custNb;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getRegId() {
+		return regId;
+	}
+
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getModId() {
+		return modId;
+	}
+
+	public void setModId(String modId) {
+		this.modId = modId;
+	}
+
+	public String getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(String modDate) {
+		this.modDate = modDate;
+	}
+
+	// hash, equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + custSeq;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (custSeq != other.custSeq)
+			return false;
+		return true;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "고객 [고객 일련번호=" + custSeq + ", 고객 아이디=" + custId + ", 이름=" + name + ", 비밀번호=" + password
+				+ ", 고객 타입=" + custType + ", 고객 코드=" + custNb + ", 이메일=" + email + ", 핸드폰=" + phone + ", 등록아이디="
+				+ regId + ", 등록일=" + regDate + ", 수정아이디=" + modId + ", 수정일=" + modDate + "]";
+	}
+	
 }
